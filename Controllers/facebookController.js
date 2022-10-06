e4ca311b90653ec6c9b65689bb0830f3271e2e55
Alfr0rax <alfr0rax@gmail.com>
2022-10-06 07:22:50 -0400

    uniendo flujo del carrusel v2

--- Controllers/facebookController.js
+++ Controllers/facebookController.js
@@ -140,6 +140,7 @@ async function receivedPostback(event) {
       console.log(
         "aaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
       );
+      respond.sendToDialogFlow(senderId, payload);
       break;
     default:
       //unindentified payload
